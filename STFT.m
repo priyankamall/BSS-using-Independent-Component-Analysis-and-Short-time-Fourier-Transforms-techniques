b947a6cf4b13ced17f0e88a3d6f0e4c100951bbb
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-09-26 15:53:15 +0530

    Update STFT.m

--- STFT.m
+++ STFT.m
@@ -35,7 +35,6 @@ nsamp=length(x);
 wlen=length(awin); %calc size and init output t-f matrix
 numtime=ceil((nsamp-wlen+1)/timestep);
 tfmat=zeros(numfreq,numtime+1);
-
 % so this loop samples x, I though x is already a sample.
 for i=1:numtime
     sind=((i-1)*timestep)+1; % current start index
